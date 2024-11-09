58c1f784661b0f9cf935b326f803e6448d15b395
LeadDeveloper(duru) <enfn2001@gmail.com>
2024-11-09 09:50:36 +0900

    NSURL 헬퍼 클래스 추가

---
+++ ios/Utils/NSURL+Helpers.h
@@ -0,0 +1,16 @@
+//
+//  NSURL+Helpers.h
+//  ThreadsProject
+//
+//  Created by LeadDeveloper on 11/9/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSURL (Helpers)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Utils/NSURL+Helpers.m
@@ -0,0 +1,12 @@
+//
+//  NSURL+Helpers.m
+//  ThreadsProject
+//
+//  Created by LeadDeveloper on 11/9/24.
+//
+
+#import "NSURL+Helpers.h"
+
+@implementation NSURL (Helpers)
+
+@end
